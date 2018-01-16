49f6c78352a01e5203c08573b69989075f8811b3
topvova <topvova@gmail.com>
2018-01-16 13:17:39 +0200

    Replace switch with polymorphism

--- src/lesson7/ChildrenPrice.java
+++ src/lesson7/ChildrenPrice.java
@@ -6,4 +6,11 @@ public class ChildrenPrice extends Price {
         return Movie.CHILDREN;
     }
 
+    public double getCharge(int daysRented) {
+         double result = 1.5;
+        if (daysRented > 3) {
+            result += (daysRented -3) * 1.5;
+        }
+        return result;
+    }
 }
