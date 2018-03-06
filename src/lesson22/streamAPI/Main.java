b0fa13b5138f5c7b7bfe9cce4cb3f3c2498bffbf
topvova <topvova@gmail.com>
2018-03-06 13:47:34 +0200

    Add tasks solutions

--- src/lesson22/streamAPI/Main.java
+++ src/lesson22/streamAPI/Main.java
@@ -32,6 +32,61 @@ public class Main {
         employees.add(new Employee("Emilia", "London", 24, 7, Position.WORKER));
         employees.add(new Employee("Emilia", "London", 24, 7, Position.WORKER));
 
+        Stream<Employee> employeesStream = employees.stream();
+        long streamLength = employees.stream().count();
+
+        // Print the last four elements of the collection
+        employeesStream.skip(streamLength - 4).forEach(System.out::println);
+
+        // Find all managers and directors in which names start at “М”
+        employeesStream.filter(x -> !x.getPosition().equals(Position.WORKER))
+                .filter(x -> x.getName().charAt(0) == 'M')
+                .forEach(System.out::println);
+
+        // Print the names of all managers using the “map” method
+        employeesStream.filter(x -> x.getPosition().equals(Position.MANAGER))
+                .map(Employee::getName) // method reference
+                .forEach(System.out::println);
+
+        // Find all the unique workers who have an experience of more than 5 years
+        employeesStream.distinct()
+                .filter(x -> x.getPosition().equals(Position.WORKER))
+                .filter(x -> x.getWorkExperience() > 5)
+                .forEach(System.out::println);
+
+        // Create a new collection that will contain all employees from London
+        List<Employee> employeesFromLondon = employeesStream.filter(x -> x.getNativeCity().equals("London"))
+                .collect(Collectors.toList());
+        for (Employee employee: employeesFromLondon) {
+            System.out.println(employee.getName());
+        }
+
+        // Print the number of employees over 30 years old
+        long employeesOver30 = employeesStream.filter(x -> x.getAge() > 30)
+                .count();
+        System.out.println(employeesOver30);
+
+        // Find the average work experience of employees.
+        Optional<Long> averageExperience = employeesStream.map(Employee::getWorkExperience)
+                .reduce((acc, x) -> acc + x)
+                .map(x -> x / streamLength);
+
+        System.out.println(averageExperience.isPresent() ? averageExperience.get() : "Not found");
+
+        // Add each employee one year of experience
+        employeesStream.peek(x -> x.setWorkExperience(x.getWorkExperience() + 1))
+                .forEach(System.out::println);
+
+        // Find all employees not from Manchester, aged 25 to 35 years
+        employeesStream.filter(x -> !x.getNativeCity().equals("Manchester"))
+                .filter(x -> x.getAge() >= 25)
+                .filter(x -> x.getAge() <= 35)
+                .forEach(System.out::println);
+
+        // Sort all employees by age
+        employeesStream.sorted(Comparator.comparing(Employee::getAge))
+                .forEach(System.out::println);
+
     }
 
 }
